a4485bb20da392e4b6febb7195e6008f58bf2b06
fengqi <124367083@qq.com>
2018-08-25 16:38:35 +0800

    add method upload

--- QiEC/latte-core/src/main/java/net/RestClient.java
+++ QiEC/latte-core/src/main/java/net/RestClient.java
@@ -8,9 +8,12 @@ import net.callback.IRequest;
 import net.callback.ISuccess;
 import net.callback.RequestCallbacks;
 
+import java.io.File;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -31,6 +34,7 @@ public class RestClient {
     private final RequestBody BODY;
     private final LoaderStyle LOADER_STYLE;
     private final Context CONTEXT;
+    private final File FILE;
 
     public RestClient(String url,
                       Map<String, Object> params,
@@ -40,7 +44,8 @@ public class RestClient {
                       IFailure failure,
                       RequestBody body,
                       LoaderStyle loaderStyle,
-                      Context context) {
+                      Context context,
+                      File file) {
         this.URL = url;
         PARAMS.putAll(params);
         this.REQUEST = request;
@@ -50,6 +55,7 @@ public class RestClient {
         this.BODY = body;
         this.LOADER_STYLE = loaderStyle;
         this.CONTEXT = context;
+        this.FILE = file;
     }
 
     public static RestClientBuilder builder() {
@@ -76,11 +82,22 @@ public class RestClient {
             case POST:
                 call = service.post(URL, PARAMS);
                 break;
+            case POST_RAW:
+                call = service.postRaw(URL, BODY);
+                break;
             case PUT:
                 call = service.put(URL, PARAMS);
                 break;
+            case PUT_RAW:
+                call = service.putRaw(URL, BODY);
+                break;
             case DELETE:
                 call = service.delete(URL, PARAMS);
+                break;
+            case UPLOAD:
+                final RequestBody requestBody = RequestBody.create(MediaType.parse(MultipartBody.FORM.toString()), FILE);
+                final MultipartBody.Part body = MultipartBody.Part.createFormData("file", FILE.getName(), requestBody);
+                call = RestCreator.getRestService().upload(URL, body);
                 break;
             default:
                 break;
@@ -106,11 +123,26 @@ public class RestClient {
     }
 
     public final void post() {
-        request(HttpMethod.POST);
+        if (BODY == null) {
+            request(HttpMethod.POST);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null!");
+            }
+            request(HttpMethod.POST_RAW);
+        }
+
     }
 
     public final void put() {
-        request(HttpMethod.PUT);
+        if (BODY == null) {
+            request(HttpMethod.PUT);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null!");
+            }
+            request(HttpMethod.PUT_RAW);
+        }
     }
 
     public final void delete() {
--- QiEC/latte-core/src/main/java/net/RestClientBuilder.java
+++ QiEC/latte-core/src/main/java/net/RestClientBuilder.java
@@ -7,6 +7,7 @@ import net.callback.IFailure;
 import net.callback.IRequest;
 import net.callback.ISuccess;
 
+import java.io.File;
 import java.util.Map;
 import java.util.WeakHashMap;
 
@@ -28,6 +29,7 @@ public class RestClientBuilder {
     private RequestBody mBody = null;
     private Context mContext = null;
     private LoaderStyle mLoaderStyle = null;
+    private File mFile = null;
 
     RestClientBuilder() {
     }
@@ -49,6 +51,16 @@ public class RestClientBuilder {
 
     public final RestClientBuilder onRequest(IRequest iRequest) {
         this.mIRequest = iRequest;
+        return this;
+    }
+
+    public final RestClientBuilder file(File file) {
+        this.mFile = file;
+        return this;
+    }
+
+    public final RestClientBuilder file(String file) {
+        this.mFile = new File(file);
         return this;
     }
 
@@ -86,6 +98,6 @@ public class RestClientBuilder {
 
 
     public final RestClient build() {
-        return new RestClient(mUrl, PARAMS, mIRequest, mISuccess, mIError, mIFailure, mBody, mLoaderStyle, mContext);
+        return new RestClient(mUrl, PARAMS, mIRequest, mISuccess, mIError, mIFailure, mBody, mLoaderStyle, mContext, mFile);
     }
 }
